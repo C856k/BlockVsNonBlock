54361499213fa68485b9e9249bae10dcc241e8e4
C856k <carl.harlang@gmail.com>
2023-11-01 17:33:10 +0100

    first commit

---
+++ src/main/java/com/example/blockingvsnonblocking/BlockingVsNonBlockingApplication.java
@@ -0,0 +1,13 @@
+package com.example.blockingvsnonblocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingVsNonBlockingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockingVsNonBlockingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/blockingvsnonblocking/dtos/Gender.java
@@ -0,0 +1,16 @@
+package com.example.blockingvsnonblocking.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Gender {
+    String gender;
+    String name;
+    int count;
+    int age;
+    double probability;
+}
